915d0b3a9151a9784780c91ab487e17552009dc1
arturo quiroz <r2inlondon@gmail.com>
2021-04-02 21:52:12 -0600

    insert book instance into table - done

--- book-list-js/app.js
+++ book-list-js/app.js
@@ -7,6 +7,9 @@ class Book {
   }  
 }
 
+// variables 
+let newBook;
+
 //Selectors
 const bookForm = document.getElementById('book-form');
 
@@ -15,32 +18,37 @@ bookForm.addEventListener('submit', addBook);
 
 // Functions
 function addBook(e){
-  let newBook;
+
   const title = document.getElementById('title').value,  
         author = document.getElementById('author').value,
         isbn = document.getElementById('isbn').value;
 
+  // validate empty fields
   if( title === "" || author === "" || isbn === ""){
     message('Fill all the fields', "error");
-    setTimeout(removeMessage, 2000);    
-    
+    setTimeout(removeMessage, 2000);        
   } else {
+    // Add success message
     message('Book added', "success");
+    // remove success message
     setTimeout(removeMessage, 2000);
+    //create instance
     newBook = new Book (title, author, isbn) ; 
-    console.log(newBook);
+    // Clear the fields    
     clearFields();
+    // insert book instance into table
+    insertBook(newBook);
   }
   e.preventDefault();
 }
 
+// success and error message
 function message(msg, classN) {
   //create div
   const msgDiv = document.createElement('div');
   msgDiv.className = `${classN} u-full-width message`;
   msgDiv.innerText = msg;
-  
-  
+    
   // insert message into DOM
   const container = document.querySelector('.container');
   container.insertBefore(msgDiv, bookForm);  
@@ -56,4 +64,26 @@ function clearFields(){
   document.getElementById('title').value = "";
   document.getElementById('author').value = "";
   document.getElementById('isbn').value = "";
+}
+
+//crete row, elements and insert book
+function insertBook(newBook){
+  // create link to delete
+  const link = document.createElement('a');
+  link.className = "delete-item";
+  link.innerHTML = '<i class="far fa-trash-alt"></i>';
+ 
+  // select table body
+  const theTable = document.querySelector('tbody'),       
+  // create row and cells
+        row = theTable.insertRow(0),
+        cell0 = row.insertCell(0),
+        cell1 = row.insertCell(1),
+        cell2 = row.insertCell(2),
+        cell3 = row.insertCell(3);        
+  // insert book instace into the cells
+        cell0.innerHTML = newBook.title;
+        cell1.innerHTML = newBook.author;
+        cell2.innerHTML = newBook.isbn;
+        cell3.appendChild(link);
 }
