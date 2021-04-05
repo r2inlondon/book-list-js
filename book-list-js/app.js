a1438239a22143b70e4c5eb4441f3bdf32f44d32
arturo quiroz <r2inlondon@gmail.com>
2021-04-04 20:07:30 -0500

    Refactored UI class and methods

--- book-list-js/app.js
+++ book-list-js/app.js
@@ -1,3 +1,7 @@
+// //Selectors
+const bookForm = document.getElementById('book-form'),
+theTable = document.getElementById('book-list');  
+
 // Book constructor
 class Book {
   constructor(title, author, isbn){
@@ -6,48 +10,90 @@ class Book {
     this.isbn = isbn;
   }  
 }
+//UI Constructor
+class UI {
+  addBookToList (book){  
+    // create table row    
+    const tr = document.createElement('tr');
+    // cells
+    tr.innerHTML = `<td>${book.title}</td>
+                    <td>${book.author}</td>
+                    <td>${book.isbn}</td>
+                    <td> <a href="#" class="delete-item"><i class="far fa-trash-alt"></i></a></td>`;
+    // insert row into the table body
+    theTable.appendChild(tr);  
+  }
+  // success and error message
+  message(msg, classN) {
+  //create div
+    const msgDiv = document.createElement('div');
+    msgDiv.className = `${classN} u-full-width message`;
+    msgDiv.innerText = msg;
+      
+    // insert message into DOM
+    const container = document.querySelector('.container');
+    container.insertBefore(msgDiv, bookForm);
+    
+    setTimeout(function(){
+      document.querySelector('.message').remove();  
+    },3000);
+  }
 
-class UI {}
+// clear fields
+  clearFields(){
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('isbn').value = "";
+  }
 
-// variables 
-let newBook;
+  // delete book
+  deleteBook(e){
+    if(e.parentElement.classList.contains('delete-item')){
+      e.parentElement.parentElement.parentElement.remove();
 
-//Selectors
-const bookForm = document.getElementById('book-form'),
-      theTable = document.getElementById('book-list');  
+      // remove from storage
+      removeInStorage(e.parentElement.parentElement.parentElement);
+    }
+  }
+}
 
-//Event Listener
+//Event Listeners
+// load from storage
 window.addEventListener('DOMContentLoaded', loadStorage);
-bookForm.addEventListener('submit', addBook);
-theTable.addEventListener('click', deleteBook);
-
-// Functions ****
-
-// Add book
-function addBook(e){
-
+// event to add book
+bookForm.addEventListener('submit', function(e){
   const title = document.getElementById('title').value,  
         author = document.getElementById('author').value,
         isbn = document.getElementById('isbn').value;
-
-  // validate empty fields
+  // instance of UI class
+  const ui = new UI();
+  // validation
   if( title === "" || author === "" || isbn === ""){
-    message('Fill all the fields', "error");      
+    ui.message('Fill all the fields', "error");      
   } else {
-    // Add success message
-    message('Book added', "success");   
     //create instance
-    newBook = new Book (title, author, isbn) ; 
-    // Clear the fields    
-    clearFields();
-    // insert book instance into table
-    insertBook(newBook);
-    // save to storage
+    const newBook = new Book (title, author, isbn) ;
+    ui.addBookToList(newBook);    
+    // Add success message
+    ui.message('Book added', "success");        
+      // Clear the fields    
+    ui.clearFields();
+      // insert book instance into table
+      // save to storage
     saveToStorage(newBook);
-
   }
   e.preventDefault();
-}
+});
+
+// delete book event
+theTable.addEventListener('click', function(e){
+  // instance of UI class
+  const ui = new UI();
+  // deleted method passing the target
+  ui.deleteBook(e.target);
+  //send message
+  ui.message('Book has been removed', 'success');
+});
 
 // Load books from storage
 function loadStorage(){
@@ -74,44 +120,6 @@ function loadStorage(){
   });
 }
 
-// success and error message
-function message(msg, classN) {
-  //create div
-  const msgDiv = document.createElement('div');
-  msgDiv.className = `${classN} u-full-width message`;
-  msgDiv.innerText = msg;
-    
-  // insert message into DOM
-  const container = document.querySelector('.container');
-  container.insertBefore(msgDiv, bookForm);
-  
-  setTimeout(function(){
-    document.querySelector('.message').remove();  
-  },3000);
-}
-
-
-// clear fields
-function clearFields(){
-  document.getElementById('title').value = "";
-  document.getElementById('author').value = "";
-  document.getElementById('isbn').value = "";
-}
-
-//crete row, elements and insert book
-function insertBook(book){
-    // create table row    
-    const tr = document.createElement('tr');
-    // cells
-    tr.innerHTML = `<td>${book.title}</td>
-                    <td>${book.author}</td>
-                    <td>${book.isbn}</td>
-                    <td> <a href="#" class="delete-item"><i class="far fa-trash-alt"></i></a></td>`;
-    // insert row into the table body
-    theTable.appendChild(tr);
-
-}
-
 // save book to storage
 function saveToStorage(newBook){
   let books;
@@ -128,15 +136,7 @@ function saveToStorage(newBook){
   localStorage.setItem('books',JSON.stringify(books));
 }
 
-// delete book
-function deleteBook (e){
-  if(e.target.parentElement.classList.contains('delete-item')){
-    e.target.parentElement.parentElement.parentElement.remove();
 
-    // remove from storage
-    removeInStorage(e.target.parentElement.parentElement.parentElement);
-  }
-}
 
 // remove book from storage
 function removeInStorage(bookItem){
