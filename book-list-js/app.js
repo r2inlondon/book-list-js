d2e2621af90d8604ac3f8ba55d604fab1062b5d2
arturo quiroz <r2inlondon@gmail.com>
2021-04-05 08:49:25 -0500

    refactored code on the Store methods

--- book-list-js/app.js
+++ book-list-js/app.js
@@ -28,39 +28,92 @@ class UI {
   //create div
     const msgDiv = document.createElement('div');
     msgDiv.className = `${classN} u-full-width message`;
-    msgDiv.innerText = msg;
-      
+    msgDiv.innerText = msg;      
     // insert message into DOM
     const container = document.querySelector('.container');
-    container.insertBefore(msgDiv, bookForm);
-    
+    container.insertBefore(msgDiv, bookForm);    
     setTimeout(function(){
       document.querySelector('.message').remove();  
     },3000);
   }
-
-// clear fields
+  //clear fields
   clearFields(){
     document.getElementById('title').value = "";
     document.getElementById('author').value = "";
     document.getElementById('isbn').value = "";
   }
-
   // delete book
   deleteBook(e){
     if(e.parentElement.classList.contains('delete-item')){
       e.parentElement.parentElement.parentElement.remove();
-
-      // remove from storage
-      removeInStorage(e.parentElement.parentElement.parentElement);
+      // // remove from storage
+      // removeInStorage(e.parentElement.parentElement.parentElement);
     }
   }
 }
 
-//Event Listeners
-// load from storage
-window.addEventListener('DOMContentLoaded', loadStorage);
-// event to add book
+// Local Storage class
+class Store {
+  static getBooks(){
+    let books;
+    //if no books in storage
+    if(localStorage.getItem('books') === null){
+      books = [];    
+    } else {
+    // get books from Storage
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;  
+  }
+
+  static displayBooks(books){
+    books.forEach(book => {    
+      // create table row    
+      const tr = document.createElement('tr');
+      // cells
+      tr.innerHTML = `<td>${book.title}</td>
+                      <td>${book.author}</td>
+                      <td>${book.isbn}</td>
+                      <td> <a href="#" class="delete-item"><i class="far fa-trash-alt"></i></a></td>`;
+      // insert row into the table body
+      theTable.appendChild(tr);
+     });   
+  }
+  
+  static saveBook(newBook){
+    const books = Store.getBooks();
+    // push new book to array of books
+    books.push(newBook);
+    // save all books to storage
+    localStorage.setItem('books',JSON.stringify(books));
+  }
+
+  static deleteBook(target){
+    const books = Store.getBooks();
+  
+    // delete book loop & conditional
+    books.forEach(function (book, index) {
+      // compara ISBNs
+      if(book.isbn === target){
+        // delete book
+        books.splice(index, 1);
+      }
+      // save to localStorage
+      localStorage.setItem('books',JSON.stringify(books));
+    });
+  }
+}
+
+
+// Event Listener to load books from storage
+window.addEventListener('DOMContentLoaded', function(){
+  // fetch books from storage
+  const books = Store.getBooks();
+  // render books
+  Store.displayBooks(books);
+});
+
+// event listener to add book
 bookForm.addEventListener('submit', function(e){
   const title = document.getElementById('title').value,  
         author = document.getElementById('author').value,
@@ -73,90 +126,28 @@ bookForm.addEventListener('submit', function(e){
   } else {
     //create instance
     const newBook = new Book (title, author, isbn) ;
+    // add book
     ui.addBookToList(newBook);    
+    // save book to storage
+    Store.saveBook(newBook);
     // Add success message
     ui.message('Book added', "success");        
       // Clear the fields    
     ui.clearFields();
-      // insert book instance into table
-      // save to storage
-    saveToStorage(newBook);
+      // insert book instance into table          
   }
   e.preventDefault();
 });
 
-// delete book event
+// delete book event listener
 theTable.addEventListener('click', function(e){
   // instance of UI class
   const ui = new UI();
-  // deleted method passing the target
+  // deleted method passing the target  
   ui.deleteBook(e.target);
+  // delete book from storage
+  Store.deleteBook(e.target.parentElement.parentElement.previousElementSibling.innerText);  
   //send message
   ui.message('Book has been removed', 'success');
 });
 
-// Load books from storage
-function loadStorage(){
-  let books;
-  //if no books in storage
-  if(localStorage.getItem('books') === null){
-    books = [];    
-  } else {
-  // get books from Storage
-    books = JSON.parse(localStorage.getItem('books'));
-  }
-  books.forEach(book => {    
-
-   // create table row    
-   const tr = document.createElement('tr');
-   // cells
-   tr.innerHTML = `<td>${book.title}</td>
-                   <td>${book.author}</td>
-                   <td>${book.isbn}</td>
-                   <td> <a href="#" class="delete-item"><i class="far fa-trash-alt"></i></a></td>`;
-   // insert row into the table body
-   theTable.appendChild(tr);
-
-  });
-}
-
-// save book to storage
-function saveToStorage(newBook){
-  let books;
-  //if no books in storage
-  if(localStorage.getItem('books') === null){
-    books = [];    
-  } else {
-  // get books from Storage
-    books = JSON.parse(localStorage.getItem('books'));
-  }
-  // push new book to array of books
-  books.push(newBook);
-  // save all books to storage
-  localStorage.setItem('books',JSON.stringify(books));
-}
-
-
-
-// remove book from storage
-function removeInStorage(bookItem){
-  let books;
-  //if no books in storage
-  if(localStorage.getItem('books') === null){
-    books = [];    
-  } else {
-  // get books from Storage
-    books = JSON.parse(localStorage.getItem('books'));
-  }
-  
-  // delete book conditional
-  books.forEach(function (book, index) {
-    if(book.title === bookItem.children[0].innerText){
-      // delete book
-      books.splice(index, 1);
-    }
-    // save to localStorage
-    localStorage.setItem('books',JSON.stringify(books));
-  });
-
-}
