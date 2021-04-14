3a315ce9ede1cf43897c18c9ad4c0247ff67f384
arturo quiroz <r2inlondon@gmail.com>
2021-04-14 13:49:09 -0500

    Local storage - completed

--- weather-api-js/app.js
+++ weather-api-js/app.js
@@ -7,6 +7,7 @@ const saveBtn = document.getElementById('w-change-btn'),
       myModalel = document.getElementById('locModal');
 
 // event listener
+window.addEventListener('DOMContentLoaded', getStorage)
 saveBtn.addEventListener('click', findCity);
 
 
@@ -18,16 +19,32 @@ function findCity(){
   if(city.value === ""){
     console.log('invalid');
   } else {
-    
+    // get data from Weather API
     weather.getWeather(city.value)
       .then(cityData => {       
         ui.showMain(cityData);
         ui.showIcon(cityData.cityWeather.weather[0].icon);                    
       });
-
+    // save to local storage
+    Store.saveCity(city.value);
   }
   // clear input field in form
   city.value = "";
+}
+
+// retriving city from Local storage
+function getStorage(){
+  let city = JSON.parse(localStorage.getItem('city'));
+
+  // set default city if Storage is empty
+  if(city === null ){
+    city = 'london';
+    Store.defaultCity(city);
+    Store.displayCity(city);
+  } else {
+    // display current city
+    Store.displayCity(city);
+  }
 }
 
 
---
+++ weather-api-js/storage.js
@@ -0,0 +1,20 @@
+class Store {
+  static defaultCity(city){
+     // set default city
+     console.log(`default City to ${city}`);
+     city = 'London';
+     localStorage.setItem('city',JSON.stringify(city));
+  }
+
+  static displayCity(city){
+    weather.getWeather(city)
+      .then(cityData => {       
+        ui.showMain(cityData);
+        ui.showIcon(cityData.cityWeather.weather[0].icon);                    
+    });
+  }
+
+  static saveCity(city){
+    localStorage.setItem('city',JSON.stringify(city));
+  }
+}
