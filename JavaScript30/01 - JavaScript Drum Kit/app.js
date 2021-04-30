7d685fb3bc4292fb30c1705b345566293ff3d59a
arturo quiroz <r2inlondon@gmail.com>
2021-04-30 18:13:12 -0500

    more vanilla js challenges

---
+++ JavaScript30/01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,28 @@
+// Listen for keys
+window.addEventListener('keydown', playDrum);
+
+// functions
+function playDrum(e){
+
+  // select the audio key
+  const drums = document.querySelector(`audio[data-key="${e.keyCode}"]`)  
+  // select the letter's buttom 
+  const keyDrum = document.querySelector(`[data-key="${e.keyCode}"]`);
+  
+  // if key doesnt match any sound, exit
+  if(!drums) return;
+  
+  // add border to the key when playing
+  keyDrum.classList += ' playing';
+
+  // to be able to play repetitive
+  drums.currentTime = 0;
+
+  // remove key border 
+  setTimeout(function(){
+    keyDrum.classList.remove('playing');
+  },45);
+  
+  // play sound
+  drums.play();
+}
