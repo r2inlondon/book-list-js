12eeb024f54794d6702d81b7e09f130dadb9fc3e
arturo quiroz <r2inlondon@gmail.com>
2021-05-02 14:25:27 -0500

    completed

--- JavaScript30/03 - CSS Variables/app.js
+++ JavaScript30/03 - CSS Variables/app.js
@@ -3,12 +3,14 @@ const inputs = document.querySelectorAll('.controls input');
 function handleChange (){
   
   // track value changes when moving mouse
-  const values = this.value || "";
+  const values = this.value;
   // get the data sizing value of input elements
-  const pixels = this.dataset
+  const pixels = this.dataset.sizing || "";
 
-
-  console.log(pixels);
+  // change value of css variables
+  document.documentElement.style
+    .setProperty(`--${this.name}`, `${values + pixels}`);
+  
 }
 
 inputs.forEach(input => input.addEventListener('change', handleChange));
