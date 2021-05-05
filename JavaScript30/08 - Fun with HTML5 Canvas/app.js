e683e693f26e1616d42ac4388172629baa72b38a
arturo quiroz <r2inlondon@gmail.com>
2021-05-05 16:34:05 -0500

    is paniting but still needs some tweaks

--- JavaScript30/08 - Fun with HTML5 Canvas/app.js
+++ JavaScript30/08 - Fun with HTML5 Canvas/app.js
@@ -7,8 +7,33 @@ window.addEventListener('load',() => {
   ctx.canvas.height = window.innerHeight;
 
   // ctx.beginPath();
-  ctx.arc(400, 275, 100, 0, 2 * Math.PI);
-  ctx.stroke(); 
 
+  let painting = false;
+
+  function startPaiting(){
+    painting = true;
+  }
+  
+  function stopPaiting(){
+    painting = false;
+    
+  }
+
+  function draw(e){
+    // exit if not painting
+    if(!painting) return;
+    // pen's thickness
+    ctx.lineWidth = 15;
+    // pen's border would be rounded
+    ctx.lineCap = "round";
+    // mouse move
+    ctx.lineTo(e.clientX, e.clientY);
+    // the actual pen's ink
+    ctx.stroke();    
+  }
+
+  canvas.addEventListener('mousedown', startPaiting);
+  canvas.addEventListener('mouseup', stopPaiting);
+  canvas.addEventListener('mousemove', draw);
 
 })
