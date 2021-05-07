ed4915cb668bd6ddf598445f9ae609a13d6326b8
arturo quiroz <r2inlondon@gmail.com>
2021-05-06 21:04:13 -0500

    skip controls

--- JavaScript30/11 - Custom Video Player/scripts.js
+++ JavaScript30/11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const theVideo = document.querySelector('.player__video'),
 
 //Event listeners
 playButton.addEventListener('click', playMovie);
-
+skipButtons.forEach(skipButton => skipButton.addEventListener('click', skipVideo));
 
 //The functions
 
@@ -15,5 +15,12 @@ function playMovie(){
   theVideo.paused ? theVideo.play() : theVideo.pause();
 }
   
-
+// skip controls
+function skipVideo(e){
+  // get the skip value from html data-skip
+  const skip = parseInt(e.target.getAttribute('data-skip'));
+  // get current video time and apply the fwd or rewind
+  vid_currentTime = theVideo.currentTime;
+  theVideo.currentTime = vid_currentTime + skip;  
+}
 
