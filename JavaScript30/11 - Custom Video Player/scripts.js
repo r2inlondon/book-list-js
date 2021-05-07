28c8ca40b36991dede802a28f2b78075cad59cab
arturo quiroz <r2inlondon@gmail.com>
2021-05-07 18:45:06 -0500

    function for the volume

--- JavaScript30/11 - Custom Video Player/scripts.js
+++ JavaScript30/11 - Custom Video Player/scripts.js
@@ -3,39 +3,40 @@ const theVideo = document.querySelector('.player__video'),
       videoScreen = document.querySelector('.viewer'),
       playButton = document.querySelector('[title="Toggle Play"]'),
       progress = document.querySelector('.progress__filled'),
+      volControl = document.querySelector('[name="volume"]'),
       skipButtons = document.querySelectorAll('[data-skip]');
 
 //Event listeners
 playButton.addEventListener('click', playMovie);
 videoScreen.addEventListener('click', playMovie);
 skipButtons.forEach(skipButton => skipButton.addEventListener('click', skipVideo));
+volControl.addEventListener('change', SetVolume);
+
 
 //The functions
 
-// plan and pause function
+// play and pause function
 function playMovie(){    
-  // console.log(theVideo.currentTime);
-  
+  // console.log(theVideo.currentTime);  
   let playProgress;
-  
-  // play and pause video on click
-  // theVideo.paused ? theVideo.play() : theVideo.pause();
-
+  // play and pause video on click  
   if(theVideo.paused){
     // play the video
     theVideo.play()
     // calling video progress bar 
     theVideo.ontimeupdate = function(){progressBar()} 
   } else {
-
     theVideo.pause();
     }
-
   //video progress bar function  
   function progressBar(){
     progress.style.flexBasis = `${(100 * (theVideo.currentTime / theVideo.duration)).toFixed(2)}%`;
-  }
-    
+  }   
+}
+
+// function for the volume
+function SetVolume(){  
+  theVideo.volume = this.value /100;
 }
   
 // skip controls
