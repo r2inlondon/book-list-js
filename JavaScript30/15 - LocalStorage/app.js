7e163e1129523423af21a2f5e2e80c7a15d65f52
arturo quiroz <r2inlondon@gmail.com>
2021-05-12 17:43:58 -0500

    item-id has been implemented for addFood function

--- JavaScript30/15 - LocalStorage/app.js
+++ JavaScript30/15 - LocalStorage/app.js
@@ -9,14 +9,21 @@ addItems.addEventListener('submit', addFood);
 
 // functions
 function addFood(e){
+  let id = itemsList.childElementCount;
   // get user inputn
   const item = addItems.querySelector('input[name=item]');  
   // create li element  
   const li = document.createElement('li');
+  // increase id if needed
+  if(itemsList.childElementCount > 0){
+    id += 1;
+  } 
+
+
   // insert input and label into li element
   li.innerHTML = `
-    <input type="checkbox" id="item">
-    <label for="item">${item.value}</label>`
+    <input type="checkbox" id="item-${id}">
+    <label for="item-${id}">${item.value}</label>`
   // insert li into the DOM
   itemsList.appendChild(li);
   // clear form
