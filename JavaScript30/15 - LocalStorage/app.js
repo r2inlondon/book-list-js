7d94768e2cac198a38576467155a26bed31176f6
arturo quiroz <r2inlondon@gmail.com>
2021-05-12 17:19:09 -0500

    add food function has been refactored

--- JavaScript30/15 - LocalStorage/app.js
+++ JavaScript30/15 - LocalStorage/app.js
@@ -13,17 +13,10 @@ function addFood(e){
   const item = addItems.querySelector('input[name=item]');  
   // create li element  
   const li = document.createElement('li');
-  // // create checkbox
-  const input = document.createElement('input');
-  input.type = "checkbox";
-  input.setAttribute('id','item');
-  // create label to display new item
-  const label = document.createElement('label');
-  label.setAttribute('for', 'item');
-  label.innerText = `${item.value}`;
-  // insert input and label into the li
-  li.appendChild(input);
-  li.appendChild(label);
+  // insert input and label into li element
+  li.innerHTML = `
+    <input type="checkbox" id="item">
+    <label for="item">${item.value}</label>`
   // insert li into the DOM
   itemsList.appendChild(li);
   // clear form
