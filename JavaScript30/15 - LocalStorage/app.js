4a7817c171e0b218d27c3712b8bd162281670f39
arturo quiroz <r2inlondon@gmail.com>
2021-05-12 18:46:36 -0500

    displayFromStorage function on page load has been implemented

--- JavaScript30/15 - LocalStorage/app.js
+++ JavaScript30/15 - LocalStorage/app.js
@@ -2,9 +2,10 @@
 const addItems = document.querySelector('.add-items'),     
       itemsList = document.querySelector('.plates');
 
-const items = [];
+let items = [];
 
 // even listener
+window.onload = displayFromStorage;                         
 addItems.addEventListener('submit', addFood);
 
 // functions
@@ -18,18 +19,51 @@ function addFood(e){
   if(items.length >= 0){
     id += 1;
   } 
-
   // insert input and label into li element
   li.innerHTML = `
     <input type="checkbox" id="item-${id -1}">
     <label for="item-${id -1}">${item.value}</label>`
-  
+  // insert new item to an array
   items.push(item.value);
+  // save to storage
+  localStorage.setItem('items',JSON.stringify(items));
   // insert li into the DOM
   itemsList.appendChild(li);
   // clear form
   item.value = "";
 
   e.preventDefault();
+}
+
+function displayFromStorage(){
+  console.log('loaded !!');
+  
+  // if no items in storage
+  if(localStorage.getItem('items') === null){
+    return;
+  } else {
+    items = JSON.parse(localStorage.getItem('items'));
+  }
+
+  let id = 0;
+    
+  items.forEach(function(item){
+
+    const li = document.createElement('li');
+    // increase id if needed
+    if(items.length > 0){
+      id += 1;
+    } else {
+      id = 0;
+    }
+    // insert input and label into li element
+    li.innerHTML = `
+      <input type="checkbox" id="item-${id -1}">
+      <label for="item-${id -1}">${item}</label>`  
+      
+      itemsList.appendChild(li);
+  });
+    
+  
 }
 
