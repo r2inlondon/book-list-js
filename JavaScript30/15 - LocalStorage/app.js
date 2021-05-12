a1438021961c95beec4492e9ae8d2ee4fc0816a5
arturo quiroz <r2inlondon@gmail.com>
2021-05-12 17:12:22 -0500

    add food function

--- JavaScript30/15 - LocalStorage/app.js
+++ JavaScript30/15 - LocalStorage/app.js
@@ -1,14 +1,34 @@
-const addItems = document.querySelector('.add-items'),
+// Selectors
+const addItems = document.querySelector('.add-items'),     
       itemsList = document.querySelector('.plates');
 
 const items = [];
 
+// even listener
 addItems.addEventListener('submit', addFood);
 
-
+// functions
 function addFood(e){
-  console.log("Added");
-
+  // get user inputn
+  const item = addItems.querySelector('input[name=item]');  
+  // create li element  
+  const li = document.createElement('li');
+  // // create checkbox
+  const input = document.createElement('input');
+  input.type = "checkbox";
+  input.setAttribute('id','item');
+  // create label to display new item
+  const label = document.createElement('label');
+  label.setAttribute('for', 'item');
+  label.innerText = `${item.value}`;
+  // insert input and label into the li
+  li.appendChild(input);
+  li.appendChild(label);
+  // insert li into the DOM
+  itemsList.appendChild(li);
+  // clear form
+  item.value = "";
 
   e.preventDefault();
 }
+
