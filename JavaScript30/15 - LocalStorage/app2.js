8999d700d9cd2a5480168d78472baf5edeb38b35
arturo quiroz <r2inlondon@gmail.com>
2021-05-13 15:26:32 -0500

    addItems and displayItems functions completed

--- JavaScript30/15 - LocalStorage/app2.js
+++ JavaScript30/15 - LocalStorage/app2.js
@@ -10,7 +10,36 @@ addItems.addEventListener('submit', addItem);
 // functions
 
 function addItem(e){
-  console.log('Item Added');
+  // get user input
+  const text = addItems.querySelector('input[name=item]').value;  
+  
+  const item = {
+    text,
+    checkbox: false
+  }
 
+  // add new item to array
+  items.push(item);
+  // insert new item into DOM
+  displayItems(items);
+  // clear form field
+  addItems.reset();
+  
   e.preventDefault();
+}
+
+function displayItems( plates = []){  
+  // save it to html and convert to string using join.
+  const html = plates.map((plate, index) => {
+    return `  
+      <li>        
+        <input type="checkbox" id="item-${index}">
+        <label for="item-${index}">${plate.text}</label>                  
+      </li> 
+    `    
+  }).join("");
+
+  // insert items into the DOM
+  itemsList.innerHTML = html;
+    
 }
