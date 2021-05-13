8bec07c638a3c2ccacee6baeb7cf1a3d6ccbb2e7
arturo quiroz <r2inlondon@gmail.com>
2021-05-13 18:30:02 -0500

    toogle checkbox

--- JavaScript30/15 - LocalStorage/app2.js
+++ JavaScript30/15 - LocalStorage/app2.js
@@ -5,26 +5,25 @@ let items = [];
 
 // even listeners                       
 addItems.addEventListener('submit', addItem);
-
+itemsList.addEventListener('click', toggleCheckbox);
 
 // functions
 
 function addItem(e){
   // get user input
   const text = addItems.querySelector('input[name=item]').value;  
-  
+  // create new item object
   const item = {
     text,
-    checkbox: false
+    done: false
   }
-
   // add new item to array
   items.push(item);
   // insert new item into DOM
   displayItems(items);
   // clear form field
   addItems.reset();
-  
+  // prevent window from reloading  
   e.preventDefault();
 }
 
@@ -33,13 +32,21 @@ function displayItems( plates = []){
   const html = plates.map((plate, index) => {
     return `  
       <li>        
-        <input type="checkbox" id="item-${index}">
+        <input type="checkbox" data-index=${index} id="item-${index}">
         <label for="item-${index}">${plate.text}</label>                  
-      </li> 
-    `    
+      </li> `    
   }).join("");
-
   // insert items into the DOM
-  itemsList.innerHTML = html;
-    
+  itemsList.innerHTML = html;  
+}
+
+function toggleCheckbox(e){
+  // if it doesnt match then exit
+  if(!e.target.matches('input')) return;
+  // grab the input element
+  const ele = e.target;
+  // Get the index from the input element
+  const index = ele.dataset.index;
+  // find item in array and update done attribute
+  items[index].done = !items[index].done;  
 }
