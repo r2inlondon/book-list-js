812fc1ef88741b42babaa553135e246420500181
arturo quiroz <r2inlondon@gmail.com>
2021-05-14 18:36:06 -0500

    completed

--- JavaScript30/18 - Adding Up Times with Reduce/app.js
+++ JavaScript30/18 - Adding Up Times with Reduce/app.js
@@ -1,11 +1,22 @@
 const videos = Array.from(document.querySelectorAll('.videos li'));
 
-const times = videos.map((video) => {
-  return parseFloat((video.dataset.time).replace(':','.'));
-});
+const seconds = videos
+  .map((video) => {
+    // return parseFloat((video.dataset.time).replace(':','.'));
+    return video.dataset.time
+    })
+    .map(timeCode => {
+      const [mins, secs] = timeCode.split(':').map(parseFloat);
+      return (mins * 60) + secs;
+    })
+    .reduce((total, vidSeconds) => total + vidSeconds);
 
-const totalTimes = times.reduce((total, num) => {
-  return total + num;
-});
+    let secondsLeft = seconds;
+    const hours = Math.floor(secondsLeft / 3600);
+    secondsLeft = secondsLeft % 3600;
 
-console.log(totalTimes);
+    const mins = Math.floor(secondsLeft / 60);
+    secondsLeft = secondsLeft % 60;
+
+    console.log(hours, mins, secondsLeft);
+
