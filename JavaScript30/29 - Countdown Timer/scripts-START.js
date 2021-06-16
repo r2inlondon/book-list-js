b9713881f7c02586bb40e424c4c0ef1f925eeba7
Arturo Quiroz <r2inlondon@gmail.com>
2021-06-16 22:22:51 +0100

    Render 'be back at' time

--- JavaScript30/29 - Countdown Timer/scripts-START.js
+++ JavaScript30/29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,6 @@
 // selectors
-const renderTimeLeft = document.querySelector('.display__time-left');
-
+const   timeLeft = document.querySelector('.display__time-left'),
+        beBackAt = document.querySelector('.display__end-time');
 // functions
 let countDown;
 
@@ -9,28 +9,39 @@ function timer(seconds){
     const now = Date.now();
     // Convert the seconds (parameter) into miliseconds
     const then = now + seconds * 1000;
-
     
-    displayTime(seconds);
+    renderTime(seconds);
     
     countDown = setInterval(() => {
 
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // stop interval when countdown gets to zero
         if(secondsLeft <= 0) clearInterval(countDown);
-                
-        displayTime(secondsLeft);
+        // function to         
+        renderTime(secondsLeft);
                     
     }, 1000);
-    
-}
 
-displayTime = seconds => {
+    const endTime = new Date(then)
+                        .toString()
+                        .match(/(\d{2}:\d{2})/)[1];
+
+    renderEnd(endTime);                        
+}
+    // Render the clock
+renderTime = seconds => {
     const hr =  Math.floor(seconds / 60/ 60);
     const min = Math.floor(seconds / 60) - (hr * 60);
     const sec = Math.floor(seconds % 60);
     // convert numbers to string and add 0 at the front when number is < 9  
-    renderTimeLeft.innerText = `${hr.toString().padStart(2,'0')}:${min.toString().padStart(2,'0')}:${sec.toString().padStart(2,'0')}`    
-    
+    timeLeft.innerText = `${hr.toString().padStart(2,'0')}:${min.toString().padStart(2,'0')}:${sec.toString().padStart(2,'0')}`    
 }
+
+    //Render be back at time
+renderEnd = endTime => {
+    beBackAt.innerText = `Be back at ${endTime}`;    
+}
+
+
+
 
