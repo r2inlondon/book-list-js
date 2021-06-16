590db8c1b3fe539beaa40f95403aa9cc93a1be8d
Arturo Quiroz <r2inlondon@gmail.com>
2021-06-16 21:26:41 +0100

    count down has been converted to string and zero has been added at the front when number is < 9

--- JavaScript30/29 - Countdown Timer/scripts-START.js
+++ JavaScript30/29 - Countdown Timer/scripts-START.js
@@ -18,8 +18,7 @@ function timer(seconds){
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         // stop interval when countdown gets to zero
         if(secondsLeft <= 0) clearInterval(countDown);
-        
-        
+                
         displayTime(secondsLeft);
                     
     }, 1000);
@@ -27,8 +26,11 @@ function timer(seconds){
 }
 
 displayTime = seconds => {
-    const min = Math.floor(seconds / 60);
+    const hr =  Math.floor(seconds / 60/ 60);
+    const min = Math.floor(seconds / 60) - (hr * 60);
     const sec = Math.floor(seconds % 60);
-    renderTimeLeft.innerText = `${min}:${sec}`
-    console.log({min, sec});
+    // convert numbers to string and add 0 at the front when number is < 9  
+    renderTimeLeft.innerText = `${hr.toString().padStart(2,'0')}:${min.toString().padStart(2,'0')}:${sec.toString().padStart(2,'0')}`    
+    
 }
+
