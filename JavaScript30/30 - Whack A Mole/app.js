55fd59aaac66ab4654512d742871e657cb09b850
Arturo Quiroz <r2inlondon@gmail.com>
2021-06-26 19:03:47 +0100

    refactored

--- JavaScript30/30 - Whack A Mole/app.js
+++ JavaScript30/30 - Whack A Mole/app.js
@@ -3,31 +3,58 @@ const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 let score = 0;
+let lastHole;
+let timeUp;
 
 // Events Listener
 moles.forEach(mole => { mole.addEventListener('click', e => {    
-    scoreBoard.innerText = `${score += 1}`;    
-    })
+    mole.parentElement.classList.remove('up');    
+    scoreBoard.innerText = `${score += 1}`;        
+    }); 
 });
 
 // functions
 const startGame = () => {
-    
+    timeUp = false;
+    scoreBoard.innerText = 0;
+    moleUp();
+
+    setTimeout(() => {
+        timeUp = true;        
+    },10000)    
 }
 
 const randTime = (min, max) => {
     return Math.round(Math.random() * (max - min) + min);    
 }
 
-function moleUp(){
-    const getHole = function(max){return Math.floor(Math.random() * max);}
-    
-    const hole = getHole(5);
 
-    holes[hole].className += " up";
+function randomHole(max){ 
+    const index =  Math.floor(Math.random() * max);
+    const hole = holes[index];
+    
+    if(hole === lastHole){
+        console.log('Is duplicated Amigo')
+        return randomHole(5);
+    };
+    
+    lastHole = hole;
+    
+    return hole
+}
+
+
+function moleUp(){
+    const time = randTime(120, 1000);
+    const hole = randomHole(5);
+
+    hole.className += " up";
 
     setTimeout(function(){        
-        holes[hole].classList.remove('up');
-    }, randTime(150,900));    
+        hole.classList.remove('up');
+        if(timeUp)return;
+        moleUp();
+    }, time);
+    
 }
 
