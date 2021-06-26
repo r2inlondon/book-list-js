b1e8e00d00dc079a7cbe5f5145dad7c36093de8c
Arturo Quiroz <r2inlondon@gmail.com>
2021-06-26 13:00:47 +0100

    moleUp

--- JavaScript30/30 - Whack A Mole/app.js
+++ JavaScript30/30 - Whack A Mole/app.js
@@ -7,3 +7,24 @@ const moles = document.querySelectorAll('.mole');
 const startGame = () => {
     
 }
+
+const randTime = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
+    
+}
+
+function moleUp(){
+    const getHole = function(max){return Math.floor(Math.random() * max);}
+    
+    const hole = getHole(5);
+
+    console.log(hole);
+
+    holes[hole].className += " up";
+
+    setTimeout(function(){        
+        holes[hole].classList.remove('up');
+    }, randTime(150,1000));
+
+    
+}
