495e1aa21633ace94f3eee57ac044c57fd845e0f
Arturo Quiroz <r2inlondon@gmail.com>
2021-06-27 14:02:34 +0100

    square frog and draw it

--- jumper-frog-js/app.js
+++ jumper-frog-js/app.js
@@ -1,22 +1,53 @@
-// selectors
+// Selectors
 const myCanvas = document.getElementById('myCanvas'),
       ctx = myCanvas.getContext('2d');
 
-// variables
-let frog = {x:200, y:200}
-let x = 140, y = 140;
+// Variables
+let x = 22, y = 12;
+let deltaX = 142, deltaY = 144;
 
-// functions
-function drawFrogPart(){
+// Event listers
+window.addEventListener('keydown', moveFrog, false);
+
+// Functions
+function drawFrog(){
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    // the frog
     ctx.beginPath();
     ctx.lineWidth = "2";
-    ctx.strokeStyle = "green";
-    ctx.rect(x, y, 10, 5);
+    ctx.strokeStyle = "#34eb98";
+    ctx.rect(deltaX, deltaY, 10, 5);
     ctx.stroke();
+    // fill color
+    ctx.fillStyle = "#34eb98";
+    ctx.fill();    
 }
 
-function startGame(){    
-    drawFrogPart();
+function moveFrog(e){
+    switch(e.keyCode) {
+        case 37:            
+            // left key pressed            
+            deltaX -= x;
+            break;
+        case 38:
+            // up key pressed            
+            deltaY -= y;
+            break;
+        case 39:
+            // right key pressed            
+            deltaX += x;
+            break;
+        case 40:
+            // down key pressed            
+            deltaY += y;
+            break;  
+    }
+    console.log({deltaX, deltaY});
+    e.preventDefault();       
+    drawFrog()
 }
 
-startGame();
+// function startGame(){    
+// }
+
+drawFrog();
