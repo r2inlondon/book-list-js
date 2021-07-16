d8be7524196396fd26bad5dce9585206e6aebef8
Arturo Quiroz <r2inlondon@gmail.com>
2021-07-16 23:14:13 +0100

    trying to get the car to move

--- jumper-frog-js/app.js
+++ jumper-frog-js/app.js
@@ -3,11 +3,9 @@ const myCanvas = document.getElementById('myCanvas'),
       ctx = myCanvas.getContext('2d');
 
 // Variables
-let x = 30, y = 15;
-let deltaX = 142, deltaY = 144;
+let x = 30, y = 15, deltaX = 142, deltaY = 144, xPos = 0, yPos = 0;    
 let keys = [];
 const gap = 15;
-
 
 // Event listers
 window.addEventListener('keydown', moveFrog);
@@ -17,7 +15,6 @@ window.addEventListener('keyup', releasedKey);
 
 function drawGrid(gap){
     ctx.beginPath();
-
     // Horizontal lines
     for(let i = 0; i < 20; i++){
         let lineY = gap + (i * gap);
@@ -30,18 +27,17 @@ function drawGrid(gap){
         let lineX = gap + (i * gap );        
         ctx.moveTo(lineX, 0);
         ctx.lineTo(lineX, 300);
-    }  
-                    
+    }                      
     ctx.lineWidth = 0.7;
     ctx.closePath();
-    ctx.stroke();
-    
+    ctx.stroke();    
 }
 
 function drawFrogImage(deltaX, deltaY){
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
     base_image = new Image();
     base_image.src = 'img/frog.svg';    
+
     base_image.onload = function(){
         ctx.drawImage(base_image, deltaX, deltaY, 20, 20);
     }
@@ -71,7 +67,7 @@ function moveFrog(e){
     
     e.preventDefault();       
     // to draw the frog on the new position
-    drawFrogImage(deltaX, deltaY);
+    drawFrogImage(deltaX, deltaY);        
     // check if you won
     didYouWin(deltaX, deltaY);
 
@@ -91,12 +87,31 @@ function didYouWin(deltaX, deltaY){
     }
 }
 
+function drawCar(xPos = 0, yPos = 0){
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    ctx.beginPath();
+    ctx.fillStyle = "#FF0000";    
+    ctx.fillRect(xPos, yPos, 30, 15);
+    
+}
+
+function moveCar(){
+    
+    while(xPos < 280 ){
+        xPos += 1;        
+        drawCar(xPos, yPos);        
+    }
+}
+
 function startGame(){
     deltaX = 127, deltaY = 129;    
-    // drawFrog();
     drawFrogImage(deltaX, deltaY);
     drawGrid(gap);
+    moveCar();
 }
 drawGrid(gap);
+
+
+
 
 
